f13134a5004b5fe9be5c3846319e75f1154362a7
Unknown <germanjugo@hotmail.com>
2018-07-10 21:57:37 -0300

    continuo subiendo modelo

--- src/main/java/model/Asignacion.java
+++ src/main/java/model/Asignacion.java
@@ -3,7 +3,16 @@ package model;
 public abstract  class Asignacion {
 
 
+private String descripcion;
+private Criterio criterio;
 
 
-    
+public Asignacion (String descripcion, Criterio criterio) {
+
+    this.descripcion=descripcion;
+    this.criterio=criterio;
+
+}
+
+
 }
