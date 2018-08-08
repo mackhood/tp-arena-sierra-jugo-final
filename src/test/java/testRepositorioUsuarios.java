31f81480cc7ae37391462785fd8309629e0877cc
Unknown <germanjugo@hotmail.com>
2018-08-07 22:26:29 -0300

    Subo testRepositorioUsuario

--- src/test/java/testRepositorioUsuarios.java
+++ src/test/java/testRepositorioUsuarios.java
@@ -1,3 +1,47 @@
+import dominio.Repositorios.RepositorioUsuarios;
+import dominio.Usuario;
+import net.sf.oval.constraint.AssertFalse;
+import net.sf.oval.constraint.AssertNull;
+import net.sf.oval.constraint.AssertNullCheck;
+import org.apache.commons.lang.ObjectUtils;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.internal.matchers.Null;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import java.util.List;
+
 public class testRepositorioUsuarios
 {
+
+
+    private RepositorioUsuarios repositorioUsuarios ;
+    private List<Usuario> usuarios = null;
+    @Before
+
+    public void setup () {
+
+
+        repositorioUsuarios = new RepositorioUsuarios();
+
+
+
+    }
+
+
+    @Test
+
+
+    public void obtenerUsuarios () {
+
+        assertThat( repositorioUsuarios.obtenerUsuarios(),is(not(usuarios)));
+
+
+    }
+
+
+
+
+
 }
